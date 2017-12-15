a19a1c71acde66e3cb3e4a175f7dbd1f12c4e7e1
xavi <xavi@delaguayaba.com>
2017-12-14 20:30:51 -0600

    Implement index view with posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   before_action :set_post, only: :show
-  def index; end
+  def index
+    @posts = Post.all
+  end
 
   def new
     @post = Post.new
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
+@user = User.create(
+    email: 'test@test.com',
+    password: 'qwerty',
+    first_name: 'John',
+    last_name: 'Snow'
+)
+
+puts "1 user created"
+
 100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale content")
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 Posts have been created"
