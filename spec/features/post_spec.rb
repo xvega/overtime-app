979d2418e5ad6ce278ae151d7f26514129ff2605
xavi <xavi@delaguayaba.com>
2017-12-17 11:32:33 -0600

    Add new post button to nav bar

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -26,6 +26,15 @@ describe 'navigate' do
 
   end
 
+  describe 'new' do
+    it 'has a link from the homepage' do
+      visit root_path
+      click_link('new_post_from_nav')
+      expect(page.status_code).to eql(200)
+    end
+
+  end
+
   describe 'creation' do
 
     before do
