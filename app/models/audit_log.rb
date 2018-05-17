255118a85ac71bb4ed05b211b08737fe8ebbbeb1
xavi <xavi@delaguayaba.com>
2018-05-16 21:31:19 -0600

    Integrate validations and defaults for audit log attr

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,3 +1,13 @@
 class AuditLog < ActiveRecord::Base
   belongs_to :user
+
+  after_initialize :set_defaults
+
+  validates_presence_of :user_id, :status, :start_date
+
+  private
+
+  def set_defaults
+    self.start_date ||= Date.today - 6.days
+  end
 end
