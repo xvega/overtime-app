d57b494f721f261243a3ad90c443195f6b9f5007
xavi <xavi@delaguayaba.com>
2018-05-16 21:36:37 -0600

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,7 +26,7 @@ puts "100 Posts have been created"
 
 
 100.times do |audit_log|
-  AuditLog.create!(user: User.last.id, status: 0, start_date: (Date.today - 6.days))
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 end
 
 puts "100 audit logs have been created"
