c223c279d9cf3fdcb51e1b12bf0952076c361e69
xavi <xavi@delaguayaba.com>
2018-05-16 22:23:54 -0600

    Implement initial view code for audit logs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :audit_logs
+  resources :audit_logs, except: %i[new edit destroy]
   namespace :admin do
     resources :users
-resources :posts
-resources :admin_users
+    resources :posts
+    resources :admin_users
 
     root to: "users#index"
   end
---
+++ spec/features/audit_log_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'AuditLog feature' do
+
+  describe 'index' do
+
+    before do
+      admin_user = FactoryBot.create(:admin_user)
+      login_as(admin_user, scope: :user)
+      FactoryBot.create(:audit_log)
+    end
+
+    it 'has an index page that can be reached' do
+      visit audit_logs_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'renders audit log content' do
+      visit audit_logs_path
+      expect(page).to have_content(/SNOW, JOHN/)
+    end
+  end
+end
