35f25681fc0a14e0eb7de4db0c682d77b503accf
xavi <xavi@delaguayaba.com>
2018-05-20 18:07:13 -0600

    Bring in live data for admin review page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,3 +1,5 @@
 class StaticController < ApplicationController
-  def homepage; end
+  def homepage
+    @pending_approvals = Post.submitted
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,10 @@ AdminUser.create(
 puts "1 AdminUser created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 3.5)
+  Post.create!(date: Date.today, rationale: "#{post} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user_id: @user.id, overtime_request: 3.5)
 end
 
 puts "100 Posts have been created"
